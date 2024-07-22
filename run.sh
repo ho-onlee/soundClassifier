69711ad607770e5f243c4711d81d9e67ca393c6c
Hoon Lee <seonhunlee@gmail.com>
2024-07-22 18:36:36 -0400

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin bash
 
 git pull
+if [ ! -d $(pwd)/soundClassifier_client/config.toml ]; then
+  cd soundClassifier_client
+  bash installer.sh
+  cd ..
+fi
 ./soundClassifier_client/.venv/bin/python3 ./soundClassifier_client
