dd79257d5398e5d0dd6cd4ad19dab47eb52ef91f
Hoon Lee <seonhunlee@gmail.com>
2024-08-01 15:01:36 -0400

    modified:   soundClassifier_client/__main__.py

--- soundClassifier_client/__main__.py
+++ soundClassifier_client/__main__.py
@@ -133,7 +133,7 @@ def main():
 
 if __name__ == '__main__':
     try:        
-        print(librosa.query_devices())
+        print(librosa.query_device())
         config = toml.load(os.path.join(os. path.dirname(__file__), 'config.toml'))
         anal = analyzer(config['Weights']['model_name'])
         if config['HOS_server']['HOS_available']:
